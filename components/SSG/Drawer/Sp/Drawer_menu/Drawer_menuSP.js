1845f12d313cc54efbedc1fc56dc4fa6c4f7b6e6
bakushin-git <bakushin.blog@gmail.com>
2025-06-08 22:29:20 +0900

    DAY71 drawer_menu（SPStyle完了これからclamp）

--- components/SSG/Drawer/Sp/Drawer_menu/Drawer_menuSP.js
+++ components/SSG/Drawer/Sp/Drawer_menu/Drawer_menuSP.js
@@ -61,12 +61,22 @@ function Header_Sp({ toggleMenu, isMenuOpen }) {
             height={125.91254197535207}
             className={styles.Drawer_menu__1}
           />
+          <img
+            src={"/DrawerMenu/Sp/1st.webp"}
+            alt="□"
+            className={styles.Drawer_menu__1Sp}
+          />
           <Image
             src={"/DrawerMenu/PC/2nd.webp"}
             alt="▽"
             width={170.0053487710697}
             height={170.0053487710697}
             className={styles.Drawer_menu__2}
+          />
+          <img
+            src={"/DrawerMenu/Sp/2nd.webp"}
+            alt="▽"
+            className={styles.Drawer_menu__2Sp}
           />
           <Image
             src={"/DrawerMenu/PC/3rd.webp"}
@@ -75,12 +85,22 @@ function Header_Sp({ toggleMenu, isMenuOpen }) {
             height={274.34326171875}
             className={styles.Drawer_menu__3}
           />
+          <img
+            src={"/DrawerMenu/Sp/3rd.webp"}
+            alt="○"
+            className={styles.Drawer_menu__3Sp}
+          />
           <Image
             src={"/DrawerMenu/PC/logo.webp"}
             alt="logo"
             width={287.1818542480469}
             height={81.00000762939453}
             className={styles.Drawer_menu__logo}
+          />
+          <img
+            src={"/DrawerMenu/Sp/logo.webp"}
+            alt="logoSp"
+            className={styles.Drawer_menu__logoSp}
           />
         </div>
         <div className={styles.Drawer_flexRight}>
