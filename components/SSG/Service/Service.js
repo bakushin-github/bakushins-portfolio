d64146a3b9f7dd19977a15e23a81dbe8e7e12629
bakushin-git <bakushin.blog@gmail.com>
2025-06-13 19:40:38 +0900

    DAY76 Service レスポンシブほぼ完了（残り 375px:矢印位置、1920px:imgずれ）

--- components/SSG/Service/Service.js
+++ components/SSG/Service/Service.js
@@ -70,6 +70,17 @@ function Service() {
             variants={fadeInMaskVariants("left")}
             onAnimationComplete={() => setLineAnimationComplete(true)}
           ></MotionImage>
+          <MotionImage
+            className={styles.lineSp}
+            src="/Service/Sp/line.webp"
+            width={86}
+            height={642}
+            alt="lineSp"
+            initial="hidden"
+            animate={isInView ? "visible" : "hidden"}
+            variants={fadeInMaskVariants("left")}
+            onAnimationComplete={() => setLineAnimationComplete(true)}
+          ></MotionImage>
           {lineAnimationComplete && (
             <>
               <MotionImage
@@ -83,11 +94,31 @@ function Service() {
                 onAnimationComplete={() => setBElementsComplete(true)}
               ></MotionImage>
               <MotionImage
+                className={styles.polygon1Sp}
+                src="/Service/Sp/polygon1.webp"
+                width={116}
+                height={61}
+                alt="polygon1Sp"
+                initial={{ opacity: 0, scale: 0.5 }}
+                animate={{ opacity: 1, scale: 1, transition: { duration: 1 } }}
+                onAnimationComplete={() => setBElementsComplete(true)}
+              ></MotionImage>
+              <MotionImage
                 className={styles.polygon2}
                 src="/Service/PC/polygon2.webp"
                 width={119}
                 height={69}
                 alt="polygon2"
+                initial={{ opacity: 0, scale: 0.5 }}
+                animate={{ opacity: 1, scale: 1, transition: { duration: 1 } }}
+                onAnimationComplete={() => setBElementsComplete(true)}
+              ></MotionImage>
+              <MotionImage
+                className={styles.polygon2Sp}
+                src="/Service/Sp/polygon2.webp"
+                width={125}
+                height={81}
+                alt="polygon2Sp"
                 initial={{ opacity: 0, scale: 0.5 }}
                 animate={{ opacity: 1, scale: 1, transition: { duration: 1 } }}
                 onAnimationComplete={() => setBElementsComplete(true)}
@@ -131,14 +162,28 @@ function Service() {
                                src="/Service/PC/hp.webp"
                 alt="hp"
                 loading="lazy"
+              />
+              <img
+                className={styles.imageSp}
+                               src="/Service/Sp/hp.webp"
+                alt="hpSp"
+                loading="lazy"
               /></div>
               <figcaption className={styles.figcaption}>
                 <Image
                   className={styles.title}
                   width={123}
-                  height={41}
+                  height={40}
                   src="/Service/PC/title_hp.webp"
                   alt="titleHp"
+                />
+                <Image
+                  className={styles.titleSp}
+                  width={104}
+                  height={41}
+                  src="/Service/Sp/title_hp.webp"
+                  alt="titleHpSp
+                  "
                 />
                 <p className={styles.caption}>
                   WordPressを活用し、オリジナルデザインのWebサイトを制作。世界中のWebサイトの約64.2%、日本では80%以上で利用される信頼性の高いツールで、直感的な更新・管理が可能です。
@@ -155,14 +200,27 @@ function Service() {
                 src="/Service/PC/ec.webp"
                 loading="lazy"
                 alt="ec"
+              />
+              <img
+                className={styles.imageSp}
+                src="/Service/Sp/ec.webp"
+                loading="lazy"
+                alt="ecSp"
               /></div>
               <figcaption className={styles.figcaption}>
                 <Image
                   className={styles.title}
-                  width={128}
-                  height={33}
+                  width={180}
+                  height={40}
                   src="/Service/PC/title_ec.webp"
                   alt="titleEc"
+                />
+                <Image
+                  className={styles.titleSp}
+                  width={155}
+                  height={41}
+                  src="/Service/Sp/title_ec.webp"
+                  alt="titleEcSp"
                 />
                 <p className={styles.caption}>
                   Shopifyを活用したECサイト構築・カスタマイズに対応。Shopifyは、世界・日本ともにシェアNo.1のECプラットフォームで、決済機能も標準搭載。スムーズな運用をサポートします。
@@ -179,13 +237,26 @@ function Service() {
                 src="/Service/PC/support.webp"
                 loading="lazy"
                 alt="support"
+              />
+              <img
+                className={styles.imageSp}
+                src="/Service/Sp/support.webp"
+                loading="lazy"
+                alt="supportSp"
               /></div>
               <figcaption className={styles.figcaption}>
                 <Image
                   className={styles.title}
-                  width={128}
-                  height={39}
+                  width={154}
+                  height={40}
                   src="/Service/PC/title_support.webp"
+                  alt="titleSupport"
+                />
+                <Image
+                  className={styles.titleSp}
+                  width={133}
+                  height={41}
+                  src="/Service/Sp/title_support.webp"
                   alt="titleSupport"
                 />
                 <p className={styles.caption}>
@@ -203,14 +274,27 @@ function Service() {
                 src="/Service/PC/coding.webp"
                 loading="lazy"
                 alt="coding"
+              />
+              <img
+                className={styles.imageSp}
+                src="/Service/Sp/coding.webp"
+                loading="lazy"
+                alt="codingSp"
               /></div>
               <figcaption className={styles.figcaption}>
                 <Image
                   className={styles.title}
-                  width={253}
-                  height={36}
+                  width={312}
+                  height={40}
                   src="/Service/PC/title_coding.webp"
                   alt="titleCoding"
+                />
+                <Image
+                  className={styles.titleSp}
+                  width={275}
+                  height={41}
+                  src="/Service/Sp/title_coding.webp"
+                  alt="titleCodingSp"
                 />
                 <p className={styles.caption}>
                   高品質なコーディングとカスタマイズに対応。JavaScriptやWordPress、Shopifyの調整・拡張も可能。リソース不足や外注ニーズにも柔軟に対応します。
