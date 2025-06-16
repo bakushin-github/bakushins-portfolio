eedaa255bc2bfcf6175915b2b80662b0697324ad
bakushin-git <bakushin.blog@gmail.com>
2025-06-16 19:53:00 +0900

    DAY79 CTA320px,blog(320px button)でTOPレスポンシブ終了

--- components/SSG/ListViewButton/ListViewButton.js
+++ components/SSG/ListViewButton/ListViewButton.js
@@ -1,14 +1,16 @@
-import React from 'react'
-import styles from "./ListViewButton.module.scss"
-import Link from 'next/link'
+import React from "react";
+import styles from "./ListViewButton.module.scss";
+import Link from "next/link";
 
 function ListViewButton({ href = "/", className }) {
   return (
     <>
-<Link href={href} className={`${styles.listLink} ${className || ''}`}>        <button className={styles.ListViewButton}>一覧をみる</button>
+      <Link href={href} className={`${styles.listLink} ${className || ""}`}>
+        {" "}
+        <button className={styles.ListViewButton}>一覧をみる</button>
       </Link>
     </>
-  )
+  );
 }
 
-export default ListViewButton
+export default ListViewButton;
