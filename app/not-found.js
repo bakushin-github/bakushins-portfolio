d82e80f97cab45773ecc068ac58afcf146f0bbb4
bakushin-git <bakushin.blog@gmail.com>
2025-06-27 15:54:53 +0900

    DAY90 レスポンシブ完了（すべてのコンポーネント）

--- app/not-found.js
+++ app/not-found.js
@@ -32,41 +32,27 @@ function not_found() {
     // ========================================================
   return (
     <div className={styles.not_found}>
-      <Image
-        className={styles.left_1stLine}
-        src="/LowerLayer/PC/left_1stLine.webp"
-        alt="left_1stLine"
-        width={439}
-        height={565}
-      />
-      <Image
-        className={styles.left_2ndLine}
-        src="/LowerLayer/PC/left_2ndLine.webp"
-        alt="left_2ndLine"
-        width={547}
-        height={350}
-      />
-      <Image
-        className={styles.right_2ndLine}
-        src="/LowerLayer/PC/right_2ndLine.webp"
-        alt="right_2ndLine"
-        width={644}
-        height={1009}
-      />
-      <Image
-        className={styles.right_polygon1}
-        src="/LowerLayer/PC/Polygon1.webp"
-        alt="right_polygon1"
-        width={232}
-        height={117}
-      />
-      <Image
-        className={styles.right_polygon2}
-        src="/LowerLayer/PC/polygon2.webp"
-        alt="right_polygon2"
-        width={239}
-        height={120}
-      />
+      <div className={styles.left_1stLineParent}>
+              <Image
+                className={styles.left_1stLine}
+                src="/LowerLayer/PC/left_1stLine.webp"
+                alt="left_1stLine"
+                fill
+              /></div>
+              <div className={styles.left_2ndLineParent}>
+              <Image
+                className={styles.left_2ndLine}
+                src="/LowerLayer/PC/left_2ndLine.webp"
+                alt="left_2ndLine"
+                fill
+              /></div>
+             <div className={styles.right_2ndParent}>
+            <Image
+              className={styles.right_2nd}
+              src="/LowerLayer/PC/right_2nd.webp"
+              alt="right_2nd"
+              fill
+            /></div>
            {windowWidth > BREAKPOINT_SP ? (
           <Header className={styles.thanksHeader} />
         ) : (
